f1e70f2bc2e0d0d8f6efa3abcaf9a021ba971331
Purvasha Mathur <forpurvashamathur@gmail.com>
2024-11-18 12:09:21 +0530

    Add files via upload

---
+++ practice/main.py
@@ -0,0 +1,10 @@
+n1= []
+
+for x in range (1500,2701):
+
+    if  (x%7==0) and (x%5==0):
+        n1.append (int(x))
+        
+
+print(n1)
+
---
+++ practice/p.py
@@ -0,0 +1,6 @@
+age= input ("enter your age : ")
+if age == (int (age)):
+   print (True)
+
+future_age = int (age) + 5
+print (future_age)
